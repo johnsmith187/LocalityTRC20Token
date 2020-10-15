c11bacd25068a93a452c6718c7a3f44fd3145ba1
regium coin <43760229+johnsmith187@users.noreply.github.com>
2020-10-15 12:00:40 +0200

    Update Locality.sol

--- Locality.sol
+++ Locality.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.5.0;
 import "./ERC20.sol";
 import "./ERC20Detailed.sol";
 
-contract Locality is ERC20, ERC20Detailed {
+contract Regium is ERC20, ERC20Detailed {
 
-    constructor () public ERC20Detailed("Locality", "LOCAL", 8) {
-        _mint(msg.sender, 31103476800 * (10 ** uint256(decimals())));
+    constructor () public ERC20Detailed("Regium", "REGI", 18) {
+        _mint(msg.sender, 7200000 * (10 ** uint256(decimals())));
     }
 }
